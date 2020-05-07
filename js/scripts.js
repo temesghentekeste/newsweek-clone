10df166ac7cf98aed52c65ccd9a7381d53834b83
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2020-05-07 15:16:33 +0300

    added scroll effect to the more-stories section, started the same for the top navbar

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,42 @@ $(document).ready(function () {
     console.log('Clicked');
     $('.mobileMenu').toggleClass('open');
   });
+
+
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    let $navbarToggler = $('.navbar-toggler');
+    let $logoContainer = $('.main-logo-container');
+    let $mainLogo = $('.main-logo');
+    let $weatherContainer = $('.weather');
+
+    console.log(position);
+    console.log($navbarToggler)
+    console.log($logoContainer)
+
+    if(position >= 300){
+      $('.red-nav').addClass('top-nav-onScroll');
+      $('.red-nav').removeClass('top-nav sticky-top');
+      $('.top-nav-container').removeClass('container');
+      $navbarToggler.css({'display': 'block'});
+      $logoContainer.css({'order': '-1', 'height': '50px'});
+      $weatherContainer.css({'margin-right': 'auto'});
+      $mainLogo.css({'height': '35px'})
+      
+    }else{
+      $('.red-nav').removeClass('top-nav-onScroll')
+      $('.top-nav-container').addClass('container');
+      $navbarToggler.css({'display': 'none'});
+      $logoContainer.css({'order': '0', 'padding-bottom': '4rem'});
+      $weatherContainer.css({'margin-right': 'inherit'});
+      $mainLogo.css({'height': '60px'})
+
+    }
+    if (position >= 1400) {
+      $('.more-stories').addClass('stick-more-stories');
+    } else {
+      $('.more-stories').removeClass('stick-more-stories');
+    }
+  });
 });
