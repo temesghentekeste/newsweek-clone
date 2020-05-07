761d9c01157ff74cca4bbdf40e4c7890e42a98d2
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2020-05-07 17:10:41 +0300

    Finished styling the scroll effect of the top navbar

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
     let $logoContainer = $('.main-logo-container');
     let $mainLogo = $('.main-logo');
     let $weatherContainer = $('.weather');
+    let $date = $('.date');
 
     console.log(position);
     console.log($navbarToggler)
@@ -22,17 +23,20 @@ $(document).ready(function () {
       $('.red-nav').removeClass('top-nav sticky-top');
       $('.top-nav-container').removeClass('container');
       $navbarToggler.css({'display': 'block'});
-      $logoContainer.css({'order': '-1', 'height': '50px'});
-      $weatherContainer.css({'margin-right': 'auto'});
-      $mainLogo.css({'height': '35px'})
+      $logoContainer.css({'order': '-1', 'height': '40px','width':'132px', 'margin-right': '1rem'});
+      $mainLogo.css({'height': '38px', 'margin-top':'0'});
+      $weatherContainer.css({'margin-right': 'auto', 'padding-top': '0.5rem'});
+      $date.css({'display':'none'});
       
     }else{
       $('.red-nav').removeClass('top-nav-onScroll')
       $('.top-nav-container').addClass('container');
       $navbarToggler.css({'display': 'none'});
-      $logoContainer.css({'order': '0', 'padding-bottom': '4rem'});
+      $logoContainer.css({'order': '0', 'height': '90px','width':'324px', 'margin-right': 'inherit'});
+      $mainLogo.css({'height': '65px', 'margin-top':'1rem'});
       $weatherContainer.css({'margin-right': 'inherit'});
-      $mainLogo.css({'height': '60px'})
+      // $mainLogo.css({'height': '60px', 'padding-bottom': '3rem'})
+      // $('.red-nav').css({'height': '95px'})
 
     }
     if (position >= 1400) {
