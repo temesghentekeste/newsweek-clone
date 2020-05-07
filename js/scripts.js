802c6d35c52a9b42ca053b29c1e6f40d3b4cbe06
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2020-05-07 18:05:39 +0300

    modified the scrolling effect of the top navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  $('.navbar-toggler').on('click', function () {
-    console.log('Clicked');
-    $('.mobileMenu').toggleClass('open');
-  });
+  // $('.navbar-toggler').on('click', function () {
+  //   console.log('Clicked');
+  //   $('.mobileMenu').toggleClass('open');
+  // });
 
 
 
@@ -18,27 +18,27 @@ $(document).ready(function () {
     console.log($navbarToggler)
     console.log($logoContainer)
 
-    if(position >= 300){
-      $('.red-nav').addClass('top-nav-onScroll');
-      $('.red-nav').removeClass('top-nav sticky-top');
-      $('.top-nav-container').removeClass('container');
-      $navbarToggler.css({'display': 'block'});
-      $logoContainer.css({'order': '-1', 'height': '40px','width':'132px', 'margin-right': '1rem'});
-      $mainLogo.css({'height': '38px', 'margin-top':'0'});
-      $weatherContainer.css({'margin-right': 'auto', 'padding-top': '0.5rem'});
-      $date.css({'display':'none'});
+    // if(position >= 300){
+    //   $('.red-nav').addClass('top-nav-onScroll');
+    //   $('.red-nav').removeClass('top-nav sticky-top');
+    //   $('.top-nav-container').removeClass('container');
+    //   $navbarToggler.css({'display': 'block'});
+    //   $logoContainer.css({'order': '-1', 'height': '40px','width':'132px', 'margin-right': '1rem', 'margn-left':'0'});
+    //   $mainLogo.css({'height': '38px', 'margin-top':'0'});
+    //   $weatherContainer.css({'margin-right': 'auto', 'padding-top': '0.5rem'});
+    //   $date.css({'display':'none'});
       
-    }else{
-      $('.red-nav').removeClass('top-nav-onScroll')
-      $('.top-nav-container').addClass('container');
-      $navbarToggler.css({'display': 'none'});
-      $logoContainer.css({'order': '0', 'height': '90px','width':'324px', 'margin-right': 'inherit'});
-      $mainLogo.css({'height': '65px', 'margin-top':'1rem'});
-      $weatherContainer.css({'margin-right': 'inherit'});
-      // $mainLogo.css({'height': '60px', 'padding-bottom': '3rem'})
-      // $('.red-nav').css({'height': '95px'})
+    // }else{
+    //   $('.red-nav').removeClass('top-nav-onScroll')
+    //   $('.top-nav-container').addClass('container');
+    //   $navbarToggler.css({'display': 'none'});
+    //   $logoContainer.css({'order': '0', 'height': '90px','width':'324px', 'margin-right': 'inherit'});
+    //   $mainLogo.css({'height': '65px', 'margin-top':'1rem'});
+    //   $weatherContainer.css({'margin-right': 'inherit'});
+    //   // $mainLogo.css({'height': '60px', 'padding-bottom': '3rem'})
+    //   // $('.red-nav').css({'height': '95px'})
 
-    }
+    // }
     if (position >= 1400) {
       $('.more-stories').addClass('stick-more-stories');
     } else {
